196c3f3737a3dc7eb65c1f5252ea0b74b9c89a3d
Augusto <Augusto@Augusto-PC>
2013-11-13 10:53:32 -0200

    Initialization and Spring Security

---
+++ grails-app/conf/BootStrap.groovy
@@ -0,0 +1,23 @@
+
+import security.Role
+import security.User
+import security.UserRole
+
+class BootStrap {
+
+    def init = { servletContext ->
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        def testUser = new User(username: 'test', password: 'password')
+        testUser.save(flush: true)
+
+        UserRole.create testUser, adminRole, true
+
+        assert User.count() == 1
+        assert Role.count() == 2
+        assert UserRole.count() == 1
+    }
+    def destroy = {
+    }
+}
