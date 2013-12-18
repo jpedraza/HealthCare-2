c17e3adfb49d22d0b086437d5215688ead85269a
Augusto <chaves.augusto@gmail.com>
2013-12-18 14:04:15 -0200

    Bootstrap for Production

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -279,6 +279,51 @@ class BootStrap {
 		
 		        UserRole.create testUser, adminRole, true   
 		        assert UserRole.count() == 2
+				break
+				
+			case Environment.DEVELOPMENT:
+				// Create Roles
+				def adminRole = Role.findOrSaveWhere(
+					authority: 'ROLE_ADMIN'
+				)
+				def userRole = Role.findOrSaveWhere(
+					authority: 'ROLE_USER'
+				)
+				assert Role.count() == 2
+				
+				def testUser = Staff.findOrSaveWhere(
+					username: 'testador',
+					password: 'testador',
+					firstName: 'testador',
+					lastName: 'testador',
+					email: 'testador',
+					birthDate: new Date(),
+					gender: Gender.MALE,
+					cpf: 'testador',
+					rg: 'testador',
+					fatherName: 'testador',
+					motherName: 'testador',
+					martialStatus: MartialStatus.Single,
+					phone: 'testador',
+					celphone: 'testador',
+					contactPhone: 'testador',
+					address: 'testador',
+					number: 123,
+					complement: 'testador',
+					city: 'testador',
+					state: State.MinasGerais,
+					zipCode: 'testador',
+					photo: new File('files/teste.txt'),
+					identification: 'testador',
+					speciality: Speciality.Speciality1
+				)
+				assert User.count() == 1
+				
+				def teste = UserRole.findOrSaveWhere(
+					user: testUser,
+					role: adminRole
+				)
+				assert UserRole.count() == 1				
 		}
     }
     def destroy = {
