3ce12595d33e9a7a78476738233f743a205c25be
Augusto <chaves.augusto@gmail.com>
2013-12-18 12:29:12 -0200

    Production Test

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,7 +12,6 @@ class BootStrap {
 				User.findAll()*.delete(flush:true);
 				Role.findAll()*.delete(flush:true);
 			
-			case Environment.PRODUCTION:
 		        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
 		        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 				assert Role.count() == 2
